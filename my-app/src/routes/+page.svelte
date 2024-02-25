546a7b70c1d7e05280142a2a0177a63627834f9f
yusong-offx <hssong1024@gmail.com>
2024-02-25 13:02:20 +0000

    remove practice parcel box and add login input

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,11 +1,8 @@
 <script>
     import "$lib/tailwind.css";
-    import ParcelBox from "$lib/components/ParcelBox.svelte";
 </script>
 
-<ParcelBox />
-
-<!-- <a
+<a
     href="/login"
     class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight"
     >/login</a
@@ -36,7 +33,7 @@
         The Zero Gravity Pen can be used to write in any orientation, including
         upside-down. It even works in outer space.
     </p>
-</div> -->
+</div>
 
 <style>
     p {
--- my-app/src/routes/login/+page.svelte
+++ my-app/src/routes/login/+page.svelte
@@ -3,20 +3,22 @@
 
 <h1>login page</h1>
 <a href="/">back</a>
-<div
-  class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
->
-  <h3
-    class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight"
-  >
-    Writes Upside-Down
-  </h3>
-  <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-    The Zero Gravity Pen can be used to write in any orientation, including
-    upside-down. It even works in outer space.
-  </p>
+<div class="login_container">
+  <label for="id"><input id="id" /></label>
+
+  <label for="password"><input id="password" /></label>
 </div>
-<h1>Hello World!</h1>
 
 <style>
+  .login_container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  input {
+    margin: 10px;
+  }
+  a {
+    margin: 10px;
+  }
 </style>
