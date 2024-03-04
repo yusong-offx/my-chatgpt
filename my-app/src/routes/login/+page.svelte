35590ff308fee95135a97d443704f11bb6eb48f5
yusong-offx <hssong1024@gmail.com>
2024-03-04 14:43:57 +0000

    make login backgroud image test

--- my-app/src/routes/login/+page.svelte
+++ my-app/src/routes/login/+page.svelte
@@ -1,12 +1,28 @@
 <script>
+  import test from "$lib/assets/test.jpg";
 </script>
 
-<div class="login_input_container">
-  <label for="id"><input id="id" /></label>
-  <label for="password"><input id="password" /></label>
+<!-- <img src={test} alt="test" /> -->
+<div class="login_background">
+  <div class="login_input_container">
+    <label for="id"><input id="id" /></label>
+    <label for="password"><input id="password" /></label>
+  </div>
 </div>
 
 <style>
+  .login_background {
+    background-image: url("$lib/assets/login_background_img.jpg");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
   .login_input_container {
     display: flex;
     flex-direction: column;
